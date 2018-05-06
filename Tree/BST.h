6a8eabc22b60a7d392a3bdd3fc662f8b1755349f
Abhishek Trivedi <atrivedi397@gmail.com>
2018-05-06 10:40:51 +0530

    Tree Completed

--- Tree/BST.h
+++ Tree/BST.h
@@ -5,34 +5,36 @@ struct node
     int data = 0;
     node* left = nullptr;
     node* right = nullptr;
-    //node* parent = nullptr;
-}*r_root , *i_root;
+}*root;
 
 
 class op_bst
 {
 public:
+    int count_nodes = 1;
     void insert_iteration(int element);
     void insert_recursive(node*& new_root , int element);
     void display_in(node*& new_root);
     void display_pre(node*& new_root);
     void display_post(node*& new_root);
-    node* r_search(node*& new_root , int data);
+    void r_search(node* new_root , int data);
     void i_search(int data);
-    void bst_min(node*& new_root);
-    void bst_max(node*& new_root);
+    void bst_min(node* new_root);
+    void bst_max(node* new_root);
+    int total_nodes(node* new_root);
+    int height(node* new_root);
 };
 
 ////*********Iteration Insert*******///////
 void op_bst::insert_iteration(int element)
 {
-    node* traverse = i_root;
+    node* traverse = root;
     node* temp2 = nullptr;
     auto temp  =  new node;
     temp->data = element;
 
     if(traverse == nullptr)
-        i_root = temp;
+        root = temp;
     else
     {
         while(traverse != nullptr)
@@ -44,9 +46,6 @@ void op_bst::insert_iteration(int element)
             else
                 traverse = traverse->left;
         }
-        //temp->parent = temp2;
-        //if(temp2 == nullptr)
-            //r_root = temp;
         if(temp->data < temp2->data)
             temp2->left = temp;
         else
@@ -102,22 +101,25 @@ void op_bst :: display_post(node*& new_root)
 
 ////////////***********Searching In A BST (Recursive)*************///////
 
-node * op_bst::r_search(node*& new_root , int data)
+void op_bst::r_search(node* new_root , int data)
 {
-    if(new_root == nullptr || new_root->data == data)
-        return new_root;
-    if(new_root->data < data)
+    if(new_root == nullptr)
+        std::cout<<"\nData not found.\n";
+    else if(new_root->data == data)
+        std::cout<<"\n\nData exits in the tree.\n"<<std::endl;
+    else if(new_root->data < data)
         return r_search(new_root->right , data);
-    else
+    else if(new_root->data > data)
         return r_search(new_root->left , data);
+    else
+        std::cout<<"\nData not found.\n";
 }
 
 ////////////***********Searching In A BST (Iterative)*************///////
 
 void op_bst::i_search(int data)
 {
-    bool flag = false;
-    node* traverse =  i_root;
+    node* traverse =  root;
     if(traverse == nullptr)
         std::cout<<"\n\nNothing to search.\n";
     else
@@ -125,22 +127,21 @@ void op_bst::i_search(int data)
         while(traverse != nullptr)
         {
             if(traverse->data == data)
-                flag = true;
+            {
+                std::cout<<"\n\nData exits in the tree.\n"<<std::endl;
+                return;
+            }
             else if(traverse->data < data)
                 traverse = traverse->right;
             else
                 traverse = traverse->left;
         }
+        std::cout<<"\nData not found.\n";
     }
-
-    if(flag)
-        std::cout<<"\n\nData exits in the tree.\n"<<std::endl;
-    else
-        std::cout<<"\n\nData not found.\n";
 }
 
 /////*********Minimum in a BST********/////
-void op_bst::bst_min(node*& new_root)
+void op_bst::bst_min(node* new_root)
 {
     while(new_root->left != nullptr)
         new_root = new_root->left;
@@ -149,16 +150,44 @@ void op_bst::bst_min(node*& new_root)
 }
 
 /////*********Maximum in a BST********/////
-void op_bst::bst_max(node*& new_root)
+void op_bst::bst_max(node* new_root)
 {
     while(new_root->right != nullptr)
         new_root = new_root->right;
 
     std::cout<<"\nThe maximum of this BST is : "<<new_root->data<<std::endl;
 }
-
-
-
-
+////////*************Total Height of a tree**********///////
+int op_bst::height(node* new_root)
+{
+    if(new_root == nullptr)
+        return 0;
+    else
+    {
+        int l_height = height(new_root->left);
+        int r_height = height(new_root->right);
+        if (l_height > r_height)
+            return l_height + 1; ///+1 is done for counting the current node
+        else
+            return r_height + 1;
+    }
+}
+/////**********Total Number of nodes is a BST*********//////////////
+int op_bst::total_nodes(node* new_root)
+{
+    if(new_root == nullptr)
+       return 0;
+    if(new_root->left != nullptr)
+    {
+        ++count_nodes;
+        count_nodes = total_nodes(new_root->left);
+    }
+    if(new_root->right != nullptr)
+    {
+        ++count_nodes;
+        count_nodes = total_nodes(new_root->right);
+    }
+       return count_nodes;
+}
 // Created by atrivedi on 4/27/18.
 
--- Tree/main.cpp
+++ Tree/main.cpp
@@ -1,37 +1,67 @@
 #include <iostream>
 #include "BST.h"
 
+void operations()
+{
+    int element , answer; op_bst object;
+    while(true)
+    {
+        std::cout<<"\nBinary Search Tree Functions:\n1. Add Node(Iteration)\n2. Add Node (Recursion)\n3. Search(Iteration)\n";
+        std::cout<<"4. Search(Recursion)\n5. Find Minimum\n6. Find Maximum\n7. Display(In Order)\n";
+        std::cout<<"8. Display(Pre Order)\n9. Display(Post Order)\n10. Find Height\n11. Total Nodes\n12. Exit\n\n";
+
+        std::cin>>answer;
+
+        switch(answer)
+        {
+            case 1 : std::cout<<"\nEnter the value:\n";
+                std::cin>>element;
+                object.insert_iteration(element);
+                break;
+
+            case 2 : std::cout<<"\nEnter the value:\n";
+                std::cin>>element;
+                object.insert_recursive(root , element);
+                break;
+
+            case 3 : std::cout<<"\nEnter element to be searched :\n";
+                std::cin>>element;
+                object.i_search(element);
+                break;
+
+            case 4 : std::cout<<"\nEnter element to be searched :\n";
+                std::cin>>element;
+                object.r_search(root, element);
+                break;
+
+            case 5 : object.bst_min(root);
+                break;
+
+            case 6 : object.bst_max(root);
+                break;
+
+            case 7 : object.display_in(root);
+                break;
+
+            case 8 : object.display_pre(root);
+                break;
+
+            case 9 : object.display_post(root);
+                break;
+
+            case 10 : std::cout<<"\nThe height is  : "<<object.height(root)<<std::endl;
+                break;
+
+            case 11 : std::cout<<"\nTotal nodes are : "<<object.total_nodes(root)<<std::endl;
+                break;
+
+            case 12 : exit(0);
+
+            default : std::cout<<"\nEnter a valid choice.\n";
+        }
+    }
+}
 int main()
 {
-    int element;
-    op_bst object;
-    object.insert_recursive(r_root , 120);
-    object.insert_recursive(r_root , 10);
-    object.insert_recursive(r_root , 1);
-    object.insert_recursive(r_root , 140);
-    object.display_in(r_root);
-    std::cout<<std::endl;
-    object.display_pre(r_root);
-    std::cout<<std::endl;
-    object.display_post(r_root);
-
-    std::cout<<"\n\n";
-    object.insert_iteration(120);
-    object.insert_iteration(10);
-    object.insert_iteration(1);
-    object.insert_iteration(140);
-    object.display_in(i_root);
-    std::cout<<std::endl;
-    object.display_pre(i_root);
-    std::cout<<std::endl;
-    object.display_post(i_root);
-
-    object.r_search(r_root , 15);
-    object.i_search(3);
-
-    object.bst_max(i_root);
-    object.bst_min(i_root);
-    object.bst_max(r_root);
-    object.bst_min(r_root);
+    operations();
 }
-
